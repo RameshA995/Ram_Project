4639f4e0162663fba73cbfe645b1bcdb2e2cefd9
RameshA995 <rameshalukapelli995@gmail.com>
2022-06-12 17:33:08 +0530

    m

---
+++ src/main/java/JavaClass2.java
@@ -0,0 +1,9 @@
+
+public class JavaClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Java class 2 is loaded");
+	}
+
+}
---
+++ src/main/java/Javaclass.java
@@ -0,0 +1,9 @@
+
+public class Javaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Java class loaded");
+	}
+
+}
